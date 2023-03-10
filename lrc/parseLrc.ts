b38a9338b42a0c11f4ce1e56910685d8b6d30020
pumbas600 <joshjeffers600@gmail.com>
2023-03-11 12:07:24 +1300

    feat: :sparkles: Parse and sort LRC lyrics

--- lrc/parseLrc.ts
+++ lrc/parseLrc.ts
@@ -1,6 +1,47 @@
 const TimestampRegex = /^((\[\d{2}:\d{2}\.\d{2}\])+).*$/g;
 
-export function parseLines(lines: string) {}
+interface LyricData {
+  timestampMs: number;
+  lyric: string;
+}
+
+export function generateSortedLrclines(lines: string[]): string[] {
+  const parsedLyrics = parseLines(lines);
+
+  const sortedLyrics = parsedLyrics
+    .sort((a, b) => a.timestampMs - b.timestampMs)
+    .map((data) => `${msToTimestamp(data.timestampMs)}${data.lyric}`);
+
+  return sortedLyrics;
+}
+
+export function parseLines(lines: string[]): LyricData[] {
+  const lyrics: LyricData[] = [];
+  for (const line of lines) {
+    const matches = line.match(TimestampRegex);
+    // Ignore non-timestamp lines (E.g. author, etc)
+    if (!matches) continue;
+
+    const endOfTimestamps = line.lastIndexOf(']');
+
+    const timestampsSubstring = line.substring(0, endOfTimestamps + 1);
+    const lyric = line.substring(endOfTimestamps + 1);
+
+    // We need to add the ending ']' back after splitting.
+    const timestamps = timestampsSubstring
+      .split(']')
+      .filter((timestamp) => timestamp.length !== 0)
+      .map((timestamp) => timestamp + ']');
+
+    // Add each timestamp to the lyrics object
+    for (const timestamp of timestamps) {
+      const ms = timestampToMs(timestamp);
+      lyrics.push({ timestampMs: ms, lyric });
+    }
+  }
+
+  return lyrics;
+}
 
 /**
  * Given a timestamp in the format: [minutes:seconds] it gets converted to ms.
@@ -10,7 +51,7 @@ export function parseLines(lines: string) {}
  * @param timestamp The timestamp to parse
  * @returns The timestamp in ms
  */
-function timestampToMs(timestamp: string): number {
+export function timestampToMs(timestamp: string): number {
   // Remove the surrounding [ ]
   timestamp = timestamp.substring(1, timestamp.length - 1);
 
@@ -23,9 +64,9 @@ function timestampToMs(timestamp: string): number {
   return ms;
 }
 
-function msToTimestamp(ms: number): string {
+export function msToTimestamp(ms: number): string {
   const minutes = Math.floor(ms / (1000 * 60));
   const seconds = (ms - minutes * 1000 * 60) / 1000;
 
-  return `[${minutes.toFixed(0)}:${minutes.toFixed(2)}]`;
+  return `[${minutes.toFixed(0).padStart(2, '0')}:${seconds.toFixed(2).padStart(5, '0')}]`;
 }
