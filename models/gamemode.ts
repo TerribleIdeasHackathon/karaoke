60562b2d0b7ec632bc210bf968e2304bc17636f2
pumbas600 <joshjeffers600@gmail.com>
2023-03-10 20:45:40 +1300

    feat: :sparkles: Create placeholder endpoints

--- models/gamemode.ts
+++ models/gamemode.ts
@@ -1,9 +1,8 @@
 import { ValuesOf } from '../types/utility';
 
-const Gamemode = {
+export const Gamemode = {
   Antonym: 'antonym',
   Themes: 'themes',
 } as const;
 
 export type Gamemode = ValuesOf<typeof Gamemode>;
-export default Gamemode;
--- src/pages/api/lyrics.ts
+++ src/pages/api/lyrics.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Music :)' });
+  res.status(200).json({ name: 'Lyrics :)' });
 }
