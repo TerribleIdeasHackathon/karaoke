5a4b0e3cba2f1233e4fd4aa9a607482455ccb0c2
pumbas600 <joshjeffers600@gmail.com>
2023-03-10 20:56:46 +1300

    feat: :sparkles: Create zod model for karaokeRequest

--- src/pages/api/lyrics.ts
+++ src/pages/api/lyrics.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: 'Lyrics :)' });
 }
