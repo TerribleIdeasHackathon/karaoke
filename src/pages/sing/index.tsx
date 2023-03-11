694c1ad6b866b4b92968a2531fd247ca55c76fd2
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2023-03-11 16:05:56 +1300

    feat: 💽 highlight code word-by-word

--- src/pages/sing/index.tsx
+++ src/pages/sing/index.tsx
@@ -1,14 +1,25 @@
 import { Container, Flex, Text } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import KaraokeLyricsCard from '../../components/KaraokeLyricsCard';
 import { KaraokeResponse } from '../../models/karaokeResponse';
 
 export default function Karaoke() {
+  const [currentWordIdx, setCurrentWordIdx] = useState(0);
   const [karaokeResponse, setKaraokeResponse] = useState<KaraokeResponse>({
     lyrics: [{ timestampMs: 0, lyric: 'Test' }],
     artist: 'Rick Astley',
     songName: 'Never gonna give you up',
   });
+
+  const sentence = 'This is a longer sentence';
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIdx((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Container>
@@ -16,7 +27,22 @@ export default function Karaoke() {
         <Text fontWeight="bold">{karaokeResponse.songName}</Text>
         <Text>{karaokeResponse.artist}</Text>
       </Flex>
-      <KaraokeLyricsCard>Test</KaraokeLyricsCard>
+      <KaraokeLyricsCard>
+        {sentence.split(' ').map((word, idx) => {
+          const isCurrentWord = idx === currentWordIdx;
+          const isNextWord = idx < currentWordIdx + 1;
+          return (
+            <Text
+              as="span"
+              key={word}
+              color={isCurrentWord ? 'red.500' : isNextWord ? 'gray.500' : 'black'}
+              fontWeight="bold"
+            >
+              {word + ' '}
+            </Text>
+          );
+        })}
+      </KaraokeLyricsCard>
     </Container>
   );
 }
