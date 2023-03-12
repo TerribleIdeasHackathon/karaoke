ed243acf886d8f146d3e2e83dd9e7c3ab62c546d
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2023-03-12 13:13:06 +1300

    refactor: 💿 improve lyric performance when animating

---
+++ src/pages/sing/useSongControls.ts
@@ -0,0 +1,72 @@
+import { KaraokeResponse } from '@/models/karaokeResponse';
+import { useAnimation } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+const textHeight = 1.875;
+
+export default function useSongControls(lyrics: KaraokeResponse['lyrics']) {
+  const controlsA = useAnimation();
+  const controlsB = useAnimation();
+  const controlsC = useAnimation();
+
+  const [index, setIndex] = useState(0);
+  const maxIdx = lyrics.length - 1;
+
+  useEffect(() => {
+    let timeout: NodeJS.Timer;
+
+    if (index < maxIdx) {
+      timeout = setTimeout(() => {
+        setIndex((index) => index + 1);
+      }, lyrics[index + 1].timestampMs - lyrics[index].timestampMs);
+    }
+
+    const defaultAnimation = {
+      y: [`0rem`, `-${textHeight}rem`, `-${textHeight}rem`],
+      transition: {
+        times: [0, 0.7, 1],
+        duration: 0.35,
+        delay: (lyrics[index].duration - 350) / 1000,
+      },
+    };
+    const halfYTransform = [`0rem`, `-${textHeight / 2}rem`, `-${textHeight / 2}rem`];
+
+    controlsA.start({
+      ...defaultAnimation,
+      opacity: [1, 0, 0],
+    });
+
+    if (index >= lyrics.length - 1) {
+      controlsB.start({
+        ...defaultAnimation,
+        y: halfYTransform,
+      });
+    } else {
+      controlsB.start({
+        ...defaultAnimation,
+      });
+    }
+
+    if (index >= lyrics.length - 1) {
+      controlsC.start({
+        opacity: 0,
+      });
+    } else {
+      controlsC.start({
+        ...defaultAnimation,
+        opacity: [0, 1, 1],
+      });
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [index, lyrics, maxIdx, controlsA, controlsB, controlsC]);
+
+  return {
+    index,
+    controlsA,
+    controlsB,
+    controlsC,
+  };
+}
