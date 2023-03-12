1fc5be9e04a64385869720ccbf032fe05c58d939
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2023-03-12 13:21:16 +1300

    refactor: 💿 change required string
    we should be getting the instrumental version, instead of the karaoke/official music video

--- src/youtube/scrapeVideo.ts
+++ src/youtube/scrapeVideo.ts
@@ -5,12 +5,14 @@ import google from 'google-it';
 
 export async function searchForSongVideo(searchTerm: string): Promise<string | null> {
   const prefix = 'site:youtube.com';
-  const officialStr = '(Official Music Video)';
-  const query = `${prefix} ${officialStr} ${searchTerm} `;
+  const requiredStr = 'instrumental';
+  const query = `${prefix} ${requiredStr} ${searchTerm} `;
   const results = (await google({ query })) as GoogleResponse;
 
   // check if the official music video is in the top 5 results
-  const officialMusicVideo = results.slice(5).find((r) => r.title.includes(officialStr) && !r.link.includes('clip'));
+  const officialMusicVideo = results
+    .slice(5)
+    .find((r) => r.title.toLowerCase().includes(requiredStr) && !r.link.includes('clip'));
 
   if (officialMusicVideo) return officialMusicVideo.link;
 
