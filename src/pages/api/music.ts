287539132d9fc583b98e3eca8ae1994729c62680
pumbas600 <joshjeffers600@gmail.com>
2023-03-12 14:33:44 +1300

    fix: :bug: Error with getting youtube Id

--- src/pages/api/music.ts
+++ src/pages/api/music.ts
@@ -10,8 +10,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { songQuery } = body;
 
   const url = await searchForSongVideo(songQuery);
+  if (url) {
+    const idStartIndex = url.indexOf('v=') + 2;
+    const idEndIndex = url.indexOf('&', idStartIndex);
+    const youtubeId = url.substring(idStartIndex, idEndIndex === -1 ? url.length - 1 : idEndIndex);
 
+    return res.status(200).json({ youtubeId });
+  }
   console.log(url);
 
-  res.status(200).json({ url });
+  res.status(404).json({ message: 'We could not find that song. Soz m8.' });
 }
--- src/pages/karaoke.tsx
+++ src/pages/karaoke.tsx
@@ -19,8 +19,8 @@ async function fetchLyrics(songQuery: string, mode: Gamemode) {
   return data;
 }
 
-async function fetchMusicUrl(songQuery: string, mode: Gamemode) {
-  const response = await fetch('/api/lyrics', {
+async function fetchYoutubeId(songQuery: string, mode: Gamemode) {
+  const response = await fetch('/api/music', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -53,12 +53,12 @@ export default function KaraokePage() {
   });
 
   const {
-    data: musicUrl,
+    data: musicData,
     isLoading: isMusicLoading,
     error: musicError,
   } = useQuery({
-    queryKey: ['karaoke', songQuery],
-    queryFn: () => fetchMusicUrl(songQuery, mode),
+    queryKey: ['music', songQuery],
+    queryFn: () => fetchYoutubeId(songQuery, mode),
   });
 
   if (isLyricsLoading || isMusicLoading) {
@@ -71,5 +71,5 @@ export default function KaraokePage() {
     return <div>Error: {JSON.stringify(error, null, 2)}</div>;
   }
 
-  return <KaraokeScreen karaokeResponse={karaokeResponse} musicUrl={musicUrl} />;
+  return <KaraokeScreen karaokeResponse={karaokeResponse} youtubeId={musicData.youtubeId} />;
 }
