613529191e1927106ceb4889972d954cab550cac
Manav Lal <manavlal5@gmail.com>
2023-03-12 16:13:14 +1300

    feat: Added randomiser to loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -10,15 +10,47 @@ const quotes = [
   'Enter at your own risk! Our loading screen is not for the weak-minded or faint of heart!',
   'You think you are tough enough for this game? Our loading screen will make you rethink your entire existence!',
   'Welcome to the world of loading screens, where your impatience and frustration will be put to the ultimate test!',
+  'Remember, switching to your secondary is faster than reloading.',
+  'Lorem Ipsum',
+  'Be wary of the Cotten Eye Joe',
+  'Warming up our vocal chords...',
+  'Finding the perfect song to match your voice...',
+  'Loading the lyrics, please hold...',
+  'Preparing to take center stage...',
+  "We're tuning up our sound system, it won't be long now...",
+  'Creating your perfect karaoke experience...',
+  'Practicing our own singing skills while you wait...',
+  "Grab some water and take a break, we'll be ready soon...",
+  'Our team of AI musicians are preparing to accompany you...',
+  'Cueing up the music and lyrics, stay tuned...',
+  'Ready to belt out your favorite tunes? We are too!',
+  'Singing in the shower is great, but karaoke is even better!',
+  "We're working hard to make your karaoke dreams come true...",
+  "We're almost ready to put you in the spotlight...",
+  'Karaoke night just got better with our AI technology.',
+  'Just a moment... or maybe two... or three...',
+  'Hold on tight, this may take a while.',
+  'Loading... loading... still loading...',
+  'Our hamsters are running as fast as they can!',
+  'Please wait while we summon the loading spirits.',
+  'Loading... because patience is a virtue, apparently.',
+  'We apologize for the delay, blame the slow internet.',
+  'Loading... please entertain yourself while we wait.',
+  'Sit back, relax, and enjoy the wait... or not.',
+  'Loading... because time is a construct anyway.',
+  'Counting to infinity... or at least it feels like it.',
+  "We'll be right back... eventually.",
+  "We're chugging along, just like that little engine that could.",
+  'Loading... because staring at a blank screen is fun, right?',
 ];
 
 export default function LoadingScreen() {
-  const [quoteIdx, setQuoteIdx] = useState(0);
+  const [quoteIdx, setQuoteIdx] = useState(Math.floor(Math.random() * quotes.length));
   const quote = quotes[quoteIdx];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setQuoteIdx((quoteIdx) => (quoteIdx + 1) % quotes.length);
+      setQuoteIdx((quoteIdx) => Math.floor(Math.random() * quotes.length));
     }, 5000);
 
     return () => clearInterval(interval);
